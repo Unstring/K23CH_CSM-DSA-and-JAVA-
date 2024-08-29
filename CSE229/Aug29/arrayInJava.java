af217efc3f72a22619a14a94de9345fb028da7f3
Amit Anand <dropmail.amit@gmail.com>
2024-08-29 13:42:05 +0530

    split in java

--- CSE229/Aug29/arrayInJava.java
+++ CSE229/Aug29/arrayInJava.java
@@ -1,5 +1,24 @@
+import java.util.*;
+
 public class arrayInJava {
     public static void main(String[] args) {
+        String nameOfString = "String";
+
+        String str = nameOfString.toUpperCase();
+        String str1 = nameOfString.toLowerCase();
+
+        char[] charArr = nameOfString.toCharArray(); // array to iterate over each character
+
+        // use foe loop and use .charAt(i) to iterate over each character
+        String charArrayString = Arrays.toString(charArr);
+
+        String names = "Rishi, Kartik, Shubham, Rahul, Shubhranjali, Komal";
+        String[] nameArray = names.split(", ");
         
+        // System.out.println(nameOfString);
+        // System.out.println(str);
+        // System.out.println(str1);
+        System.out.println(charArrayString);
+        System.out.println(Arrays.toString(nameArray));
     }
 }
