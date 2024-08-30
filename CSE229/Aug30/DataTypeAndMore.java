371882c7b010b930fdfd91d67a3f686c4d638ee5
Amit Anand <dropmail.amit@gmail.com>
2024-08-30 15:54:32 +0530

    added char casting

--- CSE229/Aug30/DataTypeAndMore.java
+++ CSE229/Aug30/DataTypeAndMore.java
@@ -5,5 +5,17 @@ public class DataTypeAndMore {
         int b = 12; // Declaration and Initialization in same line
         double d = ((double)a + (double)b) / 2; //casting int into double for accuracy
         System.out.println("(" + a + " + " + b + ") = " + d);
+
+
+        // we have two types of memory (stack and heap)
+        // where does this data initialized by int and double get stored?
+        // correct answer is stack because all premitive data types get stored in stack
+
+        // String is a class in Java, it is an object, and it gets stored in heap
+
+        int ch = 'a';
+        System.out.println("Ascii value of " + ch + " is " + ch);
+        char che = 49;
+        System.out.println("Character value of " + (int)che + " is " + che);
     }
 }
