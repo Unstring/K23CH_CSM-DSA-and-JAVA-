b5804b0f747739dfbb10197b3f7dbc02ebafc244
Amit Anand <dropmail.amit@gmail.com>
2024-08-30 16:39:58 +0530

    casual way of   making object

--- CSE229/Aug30/arraysAndStrings.java
+++ CSE229/Aug30/arraysAndStrings.java
@@ -4,13 +4,32 @@ public class arraysAndStrings {
         String str = "hello world";
         String str1 = "hello world";
         String str2 = str;
+
+        System.out.println(st);
+        System.out.println(str);
+        System.out.println(str1);
+        System.out.println(str2);
+
+        System.out.println(st == str);
+        System.out.println(st == str1);
+        System.out.println(st == str2);
+        System.out.println(str == str1);
+        System.out.println(str == str2);
+        System.out.println(str1 == str2);
+
+
         str = str + "1";
 
-        System.out.println(st == str); // false, String is object and stored in heap
-        System.out.println(st.equals(str)); // true, both string are same
+        System.out.println(st);
+        System.out.println(str);
+        System.out.println(str1);
+        System.out.println(str2);
 
-        System.out.println(str == str1); // true, both string are same and stored in string pool
-        System.out.println(str == str2); // true, both string are same and stored in string pool
-        System.out.println(str1.equals(str)); // true, both string are same
+        System.out.println(st == str);
+        System.out.println(st == str1);
+        System.out.println(st == str2);
+        System.out.println(str == str1);
+        System.out.println(str == str2);
+        System.out.println(str1 == str2);
     }
 }
