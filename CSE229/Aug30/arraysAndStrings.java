e17e909442c05c96391d5a3dda37b135c1dc0ca4
Amit Anand <dropmail.amit@gmail.com>
2024-08-30 16:28:22 +0530

    memory allocation

--- CSE229/Aug30/arraysAndStrings.java
+++ CSE229/Aug30/arraysAndStrings.java
@@ -1,9 +1,16 @@
-/**
- * arraysAndStrings
- */
 public class arraysAndStrings {
-
     public static void main(String[] args) {
-        
+        String st =  new String("hello world");
+        String str = "hello world";
+        String str1 = "hello world";
+        String str2 = str;
+        str = str + "1";
+
+        System.out.println(st == str); // false, String is object and stored in heap
+        System.out.println(st.equals(str)); // true, both string are same
+
+        System.out.println(str == str1); // true, both string are same and stored in string pool
+        System.out.println(str == str2); // true, both string are same and stored in string pool
+        System.out.println(str1.equals(str)); // true, both string are same
     }
 }
