999194cacde073b6613f60cea6f462212e4f4dc2
Amit Anand <dropmail.amit@gmail.com>
2024-08-30 15:34:18 +0530

    usecase of casting

---
+++ CSE229/Aug30/DataTypeAndMore.java
@@ -0,0 +1,8 @@
+public class DataTypeAndMore {
+    public static void main(String[] args) {
+        int a = 7;
+        int b = 12;
+        double d = (a + b) / 2;
+        System.out.println("(" + a + " + " + b + ") = " + d);
+    }
+}
